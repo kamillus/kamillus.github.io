079c7a764172bb142642852492f00b611f48aaf9
Kamil Awid <web@kamil.ca>
2020-04-12 16:19:46 -0400

    adding notebook files

---
+++ notebook/precache-manifest.0fbdc495310e36a3be4527502f1f7511.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "67a13f0ee8ec3872e1f2",
+    "url": "/static/js/app.412e8f70.chunk.js"
+  },
+  {
+    "revision": "dc6d7c46a725d6866bcd",
+    "url": "/static/js/runtime~app.44ddfa1e.js"
+  },
+  {
+    "revision": "504df38b1f6b7086df32",
+    "url": "/static/js/2.5a5bb874.chunk.js"
+  },
+  {
+    "revision": "004c2bbb035d8d06bb830efc4673c886",
+    "url": "/static/media/star.004c2bbb.png"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "5a293a273bee8d740a045d9922b9a9ae",
+    "url": "/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "6beba7e6834963f7f171d3bdd075c915",
+    "url": "/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "2327736b3ea09c41abfb69be1221f430",
+    "url": "/static/media/heart.2327736b.png"
+  },
+  {
+    "revision": "51671417ef20e0bbc32f0a2bc6edfa95",
+    "url": "/static/media/rocket.51671417.png"
+  },
+  {
+    "revision": "a7b9dc9de5f8f1fb1afbef917619a5ac",
+    "url": "/static/media/bell.a7b9dc9d.png"
+  },
+  {
+    "revision": "fb5dacb7195fba8e119940d1bb35666d",
+    "url": "/static/media/black-1072366_1920.fb5dacb7.jpg"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "4d6c5149d650dc02b60638cfa8ffbf9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bee0a6d0993a137c3cf7cae794e1797d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "04cf1ae10e30155ca207400239ec1cbd",
+    "url": "/register-service-worker.js"
+  }
+];
